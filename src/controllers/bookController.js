9da6ff9e073dc16b718346dec803b1fae9f0c20a
girija1348328 <girija7682@gmail.com>
2022-06-07 22:03:37 +0530

    output of Mongoday2

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,4 @@
-const res = require("express/lib/response")
+
 const BookModel= require("../models/bookModel")
 
 
@@ -11,16 +11,16 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-/*bookList : gives all the books- their bookName and authorName only
+//bookList : gives all the books- their bookName and authorName only
 const getBooksData= async function (req, res){
     let allBooks= await BookModel.find().select({bookName:1,authorName:1,_id : 0})
     res.send({msg: allBooks})
   }
-*/
+
 /*getBooksInYear: takes year as input in post request and gives list of all
  books published that year*/
 
- /*const getBooksInYear = async function (req,res){
+ const getBooksInYear = async function (req,res){
      let publishingYear = req.body.year
     let allBooks = await BookModel.find({year:publishingYear})
    res.send({msg:allBooks})
@@ -31,25 +31,30 @@ e.g if body had { name: “hi”} then you would fetch the books with this name
 if body had { year: 2020} then you would fetch the books in this year
 hence the condition will differ based on what you input in the request body*/
 
-/*const getParticularBooks= async function (req,res){
+const getParticularBooks= async function (req,res){
     console.log(req.body)
     let output = await BookModel.find(req.body)
     res.send(output)
-}*/
+}
 
 /*getXINRBooks- request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR”*/ 
-/*const getXINRBooks= async function (req,res){
+const getXINRBooks= async function (req,res){
     let allbooks = await BookModel.find({price:{$in:[100,200,500]} })
     res.send({msg:allbooks})
-}*/
+}
 
 /*getRandomBooks - returns books that are available in stock or have more than 500 pages */
-
+const getRandomBooks= async function (req,res)
+{
+    let allBooks = await BookModel.find({pages:{$gt:500} })
+    res.send({msg:allBooks})
+}
 
 
 
 module.exports.createBook= createBook
-//module.exports.getBooksData= getBooksData
-//module.exports.getBooksInYear= getBooksInYear
-//module.exports.getParticularBooks=getParticularBooks
-//module.exports.getXINRBooks= getXINRBooks
+module.exports.getBooksData= getBooksData
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks=getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,12 +8,13 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createUser", UserController.createUser )
 router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
 router.get("/getBooksData", BookController.getBooksData)
 router.get("/getBooksInYear", BookController.getBooksInYear)
-router.get("/getBooksInYear", BookController.getParticularBooks)
+router.get("/getParticularBooks", BookController.getParticularBooks)
 router.get("/getXINRBooks", BookController.getXINRBooks)
+router.get("/getRandomBooks", BookController.getRandomBooks)
 module.exports = router;
