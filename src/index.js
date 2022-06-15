085b3bac649862f686a5be2c79da5022b3f7d762
Girija <girija7682@gmail.com>
2022-06-15 13:19:49 +0530

    1st assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://GIRIJA:im9t78AKZG62A5PV@cluster0.j29wj.mongodb.net/girija1232", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const mw=require("../middleWare/jwtMiddleWare")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,8 +12,11 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.get("/users/:userId",mw.jwtMiddleware, userController.getUserData) //get user data Api
+
+router.put("/users/:userId",mw.jwtMiddleware, userController.updateUser) //update User data
+
+router.delete("/user/:userId",mw.jwtMiddleware,userController.deleteUser)
 
 module.exports = router;
